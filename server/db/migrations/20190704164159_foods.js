85287af521f01ddc130ea32a4830c98e6e2fc90b
Ash Foster <ajp.foster@gmail.com>
2019-07-18 10:56:03 +1200

    remove old migrations

--- server/db/migrations/20190704164159_foods.js
+++
@@ -1,9 +0,0 @@
-exports.up = knex =>
-  knex.schema.createTable('foods', (table) => {
-    table.increments('id').primary()
-    table.string('name')
-    table.integer('food_groups_id')
-      .references('food_groups.id')
-  })
-
-exports.down = knex => knex.schema.dropTable('foods')
