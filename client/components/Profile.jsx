abeb9e9162614dcd7bf14632c27fb1139992122f
Noel Soong <noelsoong@live.com>
2019-07-18 21:53:04 +1200

    WIP: Add tags for known/toKnow languages

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { Container, Grid, Card, Image, Rating } from 'semantic-ui-react'
+import { Container, Label, Flag, Card, Image, Rating } from 'semantic-ui-react'
 
 const props = {
   id: 1,
-  name: 'Benjamin',
+  name: 'Triexie',
   languages: {
-    toKnow: [],
-    know: []
+    toKnow: [{ name: 'Japanese', country: 'jp' }],
+    know: [{ name: 'Singhalise', country: 'lk' }]
   },
   description: 'asdfasdfffffffffffassdfgggggggggggggggggasdfasdfasdfasdfasdfgggdfasfaf',
   email: 'test@test.com',
@@ -19,23 +19,37 @@ function Profile () {
   return (
     <>
       <Container>
-        <Grid>
-          <Grid.Column>
-            <Card>
-              <Card.Content>
-                <Card.Header content={props.name}/>
-              </Card.Content>
-              <Card.Content content={props.description}/>
-              <Card.Content extra>
-                Learner: <Rating icon="star" defaultRating={props.ratingLearner} maxRating={5} clearable/>
-              </Card.Content>
-              <Card.Content extra>
-                Teacher: <Rating icon="star" defaultRating={props.ratingTeacher} maxRating={5} clearable/>
-              </Card.Content>
-              <Image src="https://ohgodmywifeisgerman.files.wordpress.com/2016/10/donttrusttherabbit-photo-04.jpg?w=507&h=321"/>
-            </Card>
-          </Grid.Column>
-        </Grid>
+        <Card fluid centered>
+          <Card.Header content={props.name} as="h2" textAlign="center"/>
+          <Card.Content>
+            {
+              props.languages.toKnow.map((lang, index) => (
+                <Label key={index} color="green" size="large">
+                  <Flag name={lang.country} />
+                  {lang.name}
+                </Label>
+              ))
+            }
+          </Card.Content>
+          <Card.Content>
+            {
+              props.languages.know.map((lang, index) => (
+                <Label key={index} color="red" size="large">
+                  <Flag name={lang.country} />
+                  {lang.name}
+                </Label>
+              ))
+            }
+          </Card.Content>
+          <Card.Content content={props.description} />
+          <Card.Content extra>
+            Learner: <Rating icon="star" defaultRating={props.ratingLearner} maxRating={5} clearable />
+          </Card.Content>
+          <Card.Content extra>
+            Teacher: <Rating icon="star" defaultRating={props.ratingTeacher} maxRating={5} clearable />
+          </Card.Content>
+          <Image src="https://ohgodmywifeisgerman.files.wordpress.com/2016/10/donttrusttherabbit-photo-04.jpg?w=507&h=321" />
+        </Card>
       </Container>
     </>
   )
