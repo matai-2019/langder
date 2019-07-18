54cb9d65e0c35072dc8f5660db604dae3dd81831
tamari <tamarigray97@gmail.com>
2019-07-18 16:27:38 +1200

    resolved api file

--- client/api/api.js
+++ client/api/api.js
@@ -7,10 +7,6 @@ export function login (user) {
     .send(user)
 }
 
-
-import {request} from 'supertest'
-const url = '/api/v1/users'
-
 export const getUser = (user) => {
   return request(url)
     .get(`${url}/${user}`)
