c4ee021877f16f629fbbef58ea56c6f29c8ed62b
Noel Soong <noelsoong@live.com>
2019-07-18 20:02:30 +1200

    :bug: Primary is alrealy Unsigned

--- server/db/migrations/20190718182314_profiles.js
+++ server/db/migrations/20190718182314_profiles.js
@@ -1,13 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable('profiles', table => {
-    table.increments('id').primary().unSigned()
+    table.increments('id').primary()
     table.string('name')
     table.integer('userId')
+      .unsigned()
+      .unique()
+      .notNull()
     table.foreign('userId')
       .references('user.id')
       .onDelete('CASCADE')
       .onUpdate('CASCADE')
-      .unique()
   })
 }
 
