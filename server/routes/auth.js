b36389828b05c7577ab76edbba4835ffb813073c
Noel Soong <noelsoong@live.com>
2019-07-18 18:02:07 +1200

    Refactor: Auth.js turnary is removed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,14 +6,20 @@ const router = express.Router()
 
 router.post('/login', (req, res) => {
   const loginData = req.body
+
   db.login(loginData)
     .then(user => {
-      user
-        ? res.status(200)
-          .send('TEST TOKEN')
-        : res.status(401)
+      if (user !== [] || user !== null) {
+        res.status(200)
+          .json({ auth: 'TEST TOKEN' })
+      } else {
+        res.status(401)
           .json({ code: 401, message: 'Username or email is inncorrect' })
-    }).catch(err => {
+      }
+    })
+    .catch(err => {
       res.status(500).json(err)
     })
 })
+
+module.exports = router
