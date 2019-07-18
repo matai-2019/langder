f481055765a2de096f43b88dc1cafe6e66cb16b5
Noel Soong <noelsoong@live.com>
2019-07-18 18:41:04 +1200

    Add migration for profiles

---
+++ server/db/migrations/20190718182314_profiles.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('profiles', table => {
+    table.increments('id').primary()
+    table.string('name')
+    table.integer('userId')
+    table.foreign('userId')
+      .references('user.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('profiles')
+}
