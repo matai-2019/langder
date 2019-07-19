284768557ddb675bc610286acf8981a58e00c6a9
Noel Soong <noelsoong@live.com>
2019-07-19 20:11:00 +1200

    Refactor: Material design image has to be up top

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -5,8 +5,14 @@ import { Label, Flag, Card, Image, Rating, Button } from 'semantic-ui-react'
 const props = {
   id: 1,
   name: 'Trixie',
+  image: 'https://ohgodmywifeisgerman.files.wordpress.com/2016/10/donttrusttherabbit-photo-04.jpg?w=507&h=321',
   languages: {
-    toKnow: [{ name: 'Japanese', country: 'jp' }],
+    toKnow: [
+      { name: 'Japanese', country: 'jp' },
+      { name: 'Creole', country: 'jm' },
+      { name: 'Chinese', country: 'cn' },
+      { name: 'German', country: 'de' }
+    ],
     know: [{ name: 'Singhalise', country: 'lk' }]
   },
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur possimus itaque commodi quibusdam magni. In velit, quisquam ipsa, doloremque recusandae voluptatem dolor veniam incidunt eos, dolores maxime facere quis ullam.',
@@ -18,10 +24,19 @@ const primary = '#b1f0ee'
 const secondary = '#00ffd0'
 
 const theme = {
+  header: {
+    padding: '10px 0px'
+  },
+  image: {
+    boxShadow: '1px 2px 9px -2px rgba(0,0,0,0.58)'
+  },
   card: {
     borderRadius: '10px',
-    maxWidth: '543px',
+    maxWidth: '536px',
     boxShadow: '2px 3px 14px -7px rgba(0,0,0,0.62)'
+  },
+  tag: {
+    marginBottom: '0.5em'
   },
   span: {
     fontSize: '1.25em',
@@ -38,6 +53,12 @@ const theme = {
     justifyContent: 'center',
     alignContent: 'center'
   },
+  controls: {
+    display: 'flex',
+    width: 'inherit',
+    height: '35px',
+    padding: '1em'
+  },
   button: {
     position: 'absolute',
     backgroundColor: primary,
@@ -47,7 +68,6 @@ const theme = {
     left: '25%'
   },
   deleteButton: {
-
     right: '25%'
   },
   description: {
@@ -62,7 +82,7 @@ function Profile () {
     return languages.map((lang, index) => {
       if (index <= 10) {
         return (
-          <Label key={index} color={color} size="large">
+          <Label key={index} color={color} style={theme.tag} size="large">
             <Flag name={lang.country} />
             {lang.name}
           </Label>
@@ -78,23 +98,23 @@ function Profile () {
           as="h2"
           content={props.name}
           style={theme.mainHeader} />
+        <Image src={props.image} style={theme.image}/>
         <Card.Content>
-          <Card.Header style={{ padding: '10px 0px' }}>
+          <Card.Header style={theme.header}>
             <span style={theme.span}>Teaching</span>
-            <Rating icon="star" defaultRating={props.ratingTeacher} maxRating={5} size="huge" disabled style={theme.icon} />
+            <Rating icon="star" defaultRating={props.ratingTeacher} maxRating={5} size="huge" style={theme.icon} disabled />
           </Card.Header>
           {mapLanguage(props.languages.toKnow, 'teal')}
         </Card.Content>
         <Card.Content>
-          <Card.Header style={{ padding: '10px 0px' }}>
+          <Card.Header style={theme.header}>
             <span style={theme.span}>Learning</span>
-            <Rating icon="star" defaultRating={props.ratingLearner} maxRating={5} size="huge" disabled style={theme.icon}/>
+            <Rating icon="star" defaultRating={props.ratingLearner} maxRating={5} size="huge" style={theme.icon} disabled />
           </Card.Header>
           {mapLanguage(props.languages.know)}
         </Card.Content>
-        <Image src="https://ohgodmywifeisgerman.files.wordpress.com/2016/10/donttrusttherabbit-photo-04.jpg?w=507&h=321" />
         <Card.Content content={props.description} style={theme.description} />
-        <div className="buttonControls" style={{ display: 'flex', width: 'inherit', height: '30px' }}>
+        <div className="buttonControls" style={theme.controls}>
           <Button icon='edit' size="massive" circular style={{ ...theme.button, ...theme.editButton }} />
           <Button icon='log out' size="massive" circular style={{ ...theme.button, ...theme.deleteButton }} />
         </div>
