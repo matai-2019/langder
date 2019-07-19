ffe93711afee34c9b2ee8134af65662a170ee6e5
tamari <tamarigray97@gmail.com>
2019-07-19 14:54:12 +1200

    styled labels, refactored, added textarea

--- client/components/AddProfile.jsx
+++ client/components/AddProfile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Button, Grid, Dropdown, Card } from 'semantic-ui-react'
+import { Form, Button, Grid, Dropdown, Card, TextArea } from 'semantic-ui-react'
 
 class AddProfile extends Component {
   state = {
@@ -31,32 +31,38 @@ class AddProfile extends Component {
     const langKnow = [{ key: 0, text: 'Chinese', value: 'ch' }]
 
     return (
-      <Grid verticalAlign={'middle'} style={{ height: '100vh', width: '100%' }} centered columns={3}>
+      <Grid style={{ height: '100vh', width: '100%' }} columns={1}>
         <Grid.Row style={{ display: 'flex', alignContent: 'center' }}>
-          <Card style={{ width: '75vw', maxHeight: '55vh' }}>
+          <Card centered style={{ width: '75vw', maxHeight: '85vh' }}>
             <Card.Content>
               <h1>My profile</h1>
-              <Form onSubmit={this.handleSubmit}>
+              <Form onSubmit={this.handleSubmit} style={{ height: '100%' }}>
                 <Form.Input
                   onChange={this.handleChange}
                   value={name}
-                  placeholder='Name'
+                  placeholder='Your name'
                   name='name'
+                  label='Name'
                 />
                 <Form.Input
                   value={email}
                   onChange={this.handleChange}
-                  placeholder='Email'
+                  placeholder='email@example.com'
                   name='email'
+                  label='Email'
                 />
                 <Form.Input
+                  placeholder='Tell us about yourself'
+                  name='description'
+                  label="Bio"
+                  control={TextArea}
+                  style={{ minHeight: 100 }}
                   value={description}
                   onChange={this.handleChange}
-                  placeholder='Description'
-                  name='description'
                 />
                 <Form.Field
-                  placeholder="Languages I know"
+                  placeholder="e.g English"
+                  label='Languages I know'
                   name="know"
                   control={Dropdown}
                   selection
@@ -66,7 +72,8 @@ class AddProfile extends Component {
                   value={know}
                 />
                 <Form.Field
-                  placeholder="Languages I want to learn"
+                  label='Languages I want to learn'
+                  placeholder="e.g Chinese"
                   name="toKnow"
                   control={Dropdown}
                   selection
