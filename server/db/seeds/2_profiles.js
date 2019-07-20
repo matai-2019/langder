00f03c132d4f78103bedeeca09d16d251db734f8
tamari <tamarigray97@gmail.com>
2019-07-20 23:31:06 +1200

    updated seed data to include description

--- server/db/seeds/2_profiles.js
+++ server/db/seeds/2_profiles.js
@@ -4,9 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('profiles').insert([
-        { id: 1, userId: 1, name: 'test1' },
-        { id: 2, userId: 2, name: 'test2' },
-        { id: 3, userId: 3, name: 'test3' }
+        { id: 1, userId: 1, name: 'test1', description: 'test description' },
+        { id: 2, userId: 2, name: 'test2', description: 'test description' },
+        { id: 3, userId: 3, name: 'test3', description: 'test description' }
       ])
     })
 }
