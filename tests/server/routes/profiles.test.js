d14fbcf09f1f254546b4226a0856729eb0f62259
Noel Soong <noelsoong@live.com>
2019-07-21 11:44:19 +1200

    WIP: Init profiles route test

---
+++ tests/server/routes/profiles.test.js
@@ -0,0 +1,16 @@
+import request from 'supertest'
+const server = require('../../../server/server')
+
+jest.mock('../../../server/db/db', () => ({
+  updateProfile: (profileId, profile) => Promise.resolve()
+}))
+
+describe('Tests for profiles', () => {
+  it('/api/v1/profiles/1 should update profile 1', done => {
+    request(server)
+      .put('/api/v1/profiles/1')
+      .then(() => {
+
+      })
+  })
+})
