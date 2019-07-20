7c14f7ffc53a7e1da4b7941700340d188eb38c65
Ash Foster <ajp.foster@gmail.com>
2019-07-20 17:21:28 +1200

    remove test

--- tests/server/db/profiles.test.js
+++ tests/server/db/profiles.test.js
@@ -9,36 +9,3 @@ beforeEach(() => {
 })
 
 afterEach(() => env.cleanup(testDb))
-
-test('db.updateProfile returns an updated profile', () => {
-  const profile = {
-    id: 1,
-    name: 'Yo',
-    userId: 1,
-    description: 'I have changed'
-  }
-
-  const expected = 3
-
-  return db.updateProfile(profile, testDb)
-    .then(profiles => {
-      const actual 
-      expect(actual).toBe(expected)
-    })
-})
-
-const food = {
-  id: 1,
-  name: 'Nice',
-  categoryId: 4,
-  carbonOutput: 9,
-  waterUsage: 1000
-}
-
-return db.editFood(food, testDb)
-  .then(res => {
-    expect(res.name).toBe(food.name)
-    expect(res.category).toBe('Fish')
-    expect(res.carbonOutput).toBe(food.carbonOutput)
-    expect(res.waterUsage).toBe(food.waterUsage)
-  })
