fe8f751077e6116d88aac8998e9039c98f833893
Noel Soong <noelsoong@live.com>
2019-07-21 11:31:39 +1200

    PUT Route Created for profiles

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -8,7 +8,17 @@ const router = express.Router()
 
 // post route to add profile (has ticket)
 
-// put route to update profile (has ticket)
+router.put('/:id', (req, res) => {
+  const newProfile = req.body
+  const profileId = Number(req.params.id)
+  db.updateProfile(profileId, newProfile)
+    .then(() => {
+      res.status(200).send()
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
 
 // delete route to delete profile (has ticket)
 
