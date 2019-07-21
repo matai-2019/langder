f8f9da0b0fe677df13925fe3f826c42adcb4ce19
Noel Soong <noelsoong@live.com>
2019-07-21 19:29:47 +1200

    Matches migrations have been built no errors

--- server/db/migrations/20190721183340_matches.js
+++ server/db/migrations/20190721183340_matches.js
@@ -1,8 +1,10 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('matches', table => {
-    table.increments('id')
-    table.string('user1Id')
-    table.string('user2Id')
+    table.increments('id').primary()
+    table.integer('user1Id').unsigned()
+    table.integer('user2Id').unsigned()
+    table.foreign('user1Id').references('likes.userId')
+    table.foreign('user2Id').references('likes.likedId')
   })
 }
 
