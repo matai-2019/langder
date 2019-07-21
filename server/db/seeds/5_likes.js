7c01c5cfb31533184bf51bb8511e1941c03241a6
Ash Foster <ajp.foster@gmail.com>
2019-07-21 19:29:40 +1200

    add seed

--- server/db/seeds/5_likes.js
+++ server/db/seeds/5_likes.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('userLanguages').del()
+  return knex('likes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('userLanguages').insert([
+      return knex('likes').insert([
         { id: 1, userId: 1, likedId: 2 },
         { id: 2, userId: 1, likedId: 3 },
         { id: 3, userId: 2, likedId: 3 },
