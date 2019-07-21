ebbd6a3091d41587de7927bde954f96143a59e7a
Noel Soong <noelsoong@live.com>
2019-07-21 19:34:03 +1200

    Add Delete on cascade

--- server/db/migrations/20190721183340_matches.js
+++ server/db/migrations/20190721183340_matches.js
@@ -3,8 +3,12 @@ exports.up = (knex, Promise) => {
     table.increments('id').primary()
     table.integer('user1Id').unsigned()
     table.integer('user2Id').unsigned()
-    table.foreign('user1Id').references('likes.userId')
-    table.foreign('user2Id').references('likes.likedId')
+    table.foreign('user1Id')
+      .references('likes.userId')
+      .onDelete('CASCADE')
+    table.foreign('user2Id')
+      .references('likes.likedId')
+      .onDelete('CASCADE')
   })
 }
 
