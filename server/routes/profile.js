102c7fd49dfd0891da434ff516b9ef96bcb67c4f
Noel Soong <noelsoong@live.com>
2019-07-21 13:17:39 +1200

    Get profile route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -7,6 +7,12 @@ const router = express.Router()
 // get route to get profile (has ticket)
 
 // post route to add profile (has ticket)
+router.get('/:id', (req, res) => {
+  const profileId = Number(req.params.id)
+  db.getProfile(profileId)
+    .then(profile => res.status(200).json(profile))
+    .catch(err => res.status(500).json(err))
+})
 
 // put route to update profile (has ticket)
 
