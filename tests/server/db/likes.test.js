90f9181d947bacc6dd39f6367fb6478f5f0e3bc5
Ash Foster <ajp.foster@gmail.com>
2019-07-21 21:07:09 +1200

    test and test seed

---
+++ tests/server/db/likes.test.js
@@ -0,0 +1,21 @@
+const env = require('./test-environment')
+const db = require('../../../server/db/db')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
+})
+
+afterEach(() => env.cleanup(testDb))
+
+test('db.getUserLikes returns all a users likes', () => {
+  const userId = 1
+  return db.getUserLikes(userId, testDb)
+    .then(likes => {
+      console.log(likes)
+      const actual = likes.length
+      expect(actual).toBe(2)
+    })
+})
