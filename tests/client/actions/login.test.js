cb0c07e8986df7f1dccb9cfea02ecb579cd66d3d
tamari <tamarigray97@gmail.com>
2019-07-22 16:13:13 +1200

    tidied up action and reducer to login

--- tests/client/actions/login.test.js
+++ tests/client/actions/login.test.js
@@ -1,13 +1,12 @@
-
 import { loginSuccess, LOGIN_SUCCESS } from '../../../client/actions/login'
 
 describe('actions', () => {
   it('should create a successful login action', () => {
-    const testUser = { userName: 'balls', password: 'password'}
+    const testUser = { userName: 'balls', password: 'password' }
     const action = loginSuccess(testUser)
     const expected = {
       type: LOGIN_SUCCESS,
-      user: { userName: 'balls', password: 'password'}
+      user: { userName: 'balls', password: 'password' }
     }
     expect(action).toEqual(expected)
   })
