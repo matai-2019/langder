8cf3f73717b95fa794e4092ccf3237079180be1f
David Nicholson <davidnicholsonjob@gmail.com>
2019-07-22 17:18:14 +1200

    checkit

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -11,14 +11,16 @@ class Login extends Component {
 
   handleSubmit = () => this.setState({ email: '', password: '' })
 
+  // handleSignUp
+  
   render() {
     const { email, password } = this.state
     const inputStyle = { width: '60vw' }
 
     return (
-      <Grid style={{ marginTop: '30vh' }} container centered columns={1}>
-        <Grid.Row verticalAlign='middle' centered columns={1}>
-          <h1>Login</h1>
+      <Grid style={{ marginTop: '5vh' }} container centered columns={1}>
+        <Grid.Row verticalAlign="middle" centered columns={1}>
+          <h1>This can be a title for the App. With image of Trixie etc.</h1>
           <Form onSubmit={this.handleSubmit}>
             <Form.Input
               style={inputStyle}
@@ -34,7 +36,12 @@ class Login extends Component {
               placeholder='Password'
               name='password'
             />
-            <Button type='submit'>Submit</Button>
+          <div class="ui buttons">
+            <Button onClick={this.signUp} class="ui button" type='submit' verticalAlign='middle' size="huge">Login</Button>
+              <div class="or" size="massive"></div>
+            <Button onClick={this.login} class="ui positive button" type='signup' size="huge">SignUp</Button>
+          </div>
+            {/* <Button type='submit'>Submit</Button> */}
           </Form>
         </Grid.Row>
       </Grid>
