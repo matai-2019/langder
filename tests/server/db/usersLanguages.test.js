6957381ad5da24f5f5295d6826559c7e3872a449
tamari <tamarigray97@gmail.com>
2019-07-22 15:29:03 +1200

    finished db test

--- tests/server/db/usersLanguages.test.js
+++ tests/server/db/usersLanguages.test.js
@@ -14,7 +14,7 @@ afterEach(() => env.cleanup(testDb))
 test('db.addUserLanguage adds 3 rows to userLanguages table', () => {
   expect.assertions(1)
 
-  const expected = 6
+  const expected = 7
   const userId = 1
   const languagesArray = [1, 2, 3]
 
@@ -30,7 +30,7 @@ test('db.getUserLanguages get a specific users languages', (done) => {
 
   db.getUserLanguages(userId, testDb)
     .then(userlanguages => {
-      expect(userlanguages.length).toBe(1)
+      expect(userlanguages.length).toBe(2)
       done()
     })
 })
