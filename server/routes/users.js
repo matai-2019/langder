1a8df9619a9fcb64e8833bfebe28b9161b020c33
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-22 12:18:40 +1200

    Tested is correct

--- server/routes/users.js
+++ server/routes/users.js
@@ -53,7 +53,7 @@ router.delete('/:id', (req, res) => {
 
   db.deleteUser(userId)
     .then(() => {
-      res.status(204).json()
+      res.status(200).json({ Okay: true })
     })
     .catch(err => {
       res.status(500).json(err)
