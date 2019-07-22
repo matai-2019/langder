ba8c7e15e4e055961c61d2a149ff6dd9845e3165
Noel Soong <noelsoong@live.com>
2019-07-22 21:47:06 +1200

    Add hashing in seeds

--- tests/server/db/seeds/1_users.js
+++ tests/server/db/seeds/1_users.js
@@ -2,9 +2,9 @@ exports.seed = function (knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        { id: 1, email: 'email@email.com', password: 'password' },
-        { id: 2, email: 'email2@email.com', password: 'password' },
-        { id: 3, email: 'email3@email.com', password: 'password' }
+        { id: 1, email: 'email@email.com', password: '$2b$10$fWmEfnYjhGXW2XTP//PcTupPqg7vZmoTU9Rt2LsqxRtAfNs9juV22' },
+        { id: 2, email: 'email2@email.com', password: '$2b$10$fWmEfnYjhGXW2XTP//PcTupPqg7vZmoTU9Rt2LsqxRtAfNs9juV22' },
+        { id: 3, email: 'email3@email.com', password: '$2b$10$fWmEfnYjhGXW2XTP//PcTupPqg7vZmoTU9Rt2LsqxRtAfNs9juV22' }
       ])
     })
 }
