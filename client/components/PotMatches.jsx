38e3a477cf41a1faff3598519b8442d0bee8af5e
David Nicholson <davidnicholsonjob@gmail.com>
2019-07-23 17:05:49 +1200

    potreload

--- client/components/PotMatches.jsx
+++ client/components/PotMatches.jsx
@@ -5,40 +5,47 @@ import Profile from './Profile'
 
 import { likePotMatch, nextPotMatch, fetchPotMatches } from '../actions/potMatches'
 
+//consistant styles
+const primary = '#b1f0ee'
+const secondary = '#00ffd0'
+
+const theme = {
+  button: {
+    position: 'flex',
+    backgroundColor: primary,
+    boxShadow: '2px 3px 14px -7px rgba(0,0,0,0.62)'
+  },
+}
+
+
 class PotMatches extends React.Component {
   componentDidMount () {
     this.props.dispatch(fetchPotMatches())
   }
-
+  
   render () {
     const { activePot, nextPot, dispatch } = this.props
     return (
       <>
         <Container className='matches'>
           <h1>Your Matches are all Trixie. nice.</h1>
-          ? {activePot && <Profile user={activePot} />}
-            {nextPot && <Profile user={nextPot} />}
-          : <div class="ui placeholder">
+          {activePot && <Profile user={activePot} />}
+          {nextPot && <Profile user={nextPot} />}
+          <div class="ui placeholder">
               <div class="image">Nein</div>
             </div>
           {/* MEssage underneith that says we're all done */}
-          <Button
-            name='like'
-            onClick={() => dispatch(likePotMatch(activePot))}>
-            <Icon name='like'/>
-          </Button>
-          <Button
-            name='reject'
-            onClick={() => dispatch(nextPotMatch())}>
-            <Icon name='close'/>
-          </Button>
-          <Button
-            name='cloud download' name='ui active inline loader'
-            onClick={() => dispatch(fetchPotMatches())}>
-            <Icon name='cloud download'/>
-            <Loader name='ui active inline loader' />
-          </Button>
+
+          <Button floated='left' icon='like' size="huge" circular style={{ ...theme.button }}
+            onClick={() => dispatch(likePotMatch(activePot))} />
+
+          <Button floated='right' icon='close' size="huge" circular style={{ ...theme.button }} 
+            onClick={() => dispatch(nextPotMatch())} />
+
+        <Button fix='center' icon='cloud download' size="huge" circular style={{ ...theme.button }} onClick={() => dispatch(fetchPotMatches())} />
+        
         </Container>
+        
       </>
     )
   }
