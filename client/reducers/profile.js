de9dfbf22495ff25f427afd19f77d2af6c629ddd
tamari <tamarigray97@gmail.com>
2019-07-24 13:40:40 +1200

    modified reducer

--- client/reducers/profile.js
+++ client/reducers/profile.js
@@ -12,26 +12,28 @@ export default function profile (state = {}, action) {
   switch (action.type) {
     case PENDING_UPDATE_PROFILE:
       return {
-        pending: true
+        ...state, pending: true
       }
     case UPDATE_PROFILE_SUCCESS:
       return {
-        user: action.profile,
+        ...state,
         pending: false
       }
-
     case UPDATE_PROFILE_ERROR:
       return {
+        ...state,
         error: action.error
       }
     case PENDING_GET_PROFILE:
       return {
+        ...state,
         pending: true
       }
     case GET_PROFILE_SUCCESS:
       return { ...state, ...action.profile, pending: false }
     case GET_PROFILE_ERROR:
       return {
+        ...state,
         error: action.error
       }
     case GET_LANGUAGES_SUCCESS:
