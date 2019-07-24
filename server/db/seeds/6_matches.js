6589068a3eb3ae2175e6ffeae78e59c5f789cd26
tamari <tamarigray97@gmail.com>
2019-07-24 21:17:54 +1200

    updated seeds

--- server/db/seeds/6_matches.js
+++ server/db/seeds/6_matches.js
@@ -2,7 +2,7 @@ exports.seed = function (knex) {
   return knex('matches').del()
     .then(function () {
       return knex('matches').insert([
-        { id: 1, user1Id: 1, user2Id: 2 },
+        { id: 1, user1Id: 1, user2Id: 8 },
         { id: 2, user1Id: 1, user2Id: 3 },
         { id: 3, user1Id: 4, user2Id: 5 }
       ])
