47d0e9563a530cb5c786307dd8a70083d06732a1
David Nicholson <davidnicholsonjob@gmail.com>
2019-07-24 14:45:26 +1200

    here

--- server/db/seeds/2_profiles.js
+++ server/db/seeds/2_profiles.js
@@ -2,38 +2,19 @@ exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('profiles').del()
     .then(function () {
-      // Inserts seed entries
+      // Inserts seed entries 
       return knex('profiles').insert([
-        { id: 1, userId: 1, name: 'test1', description: 'test description' },
-        { id: 2, userId: 2, name: 'test2', description: 'test description' },
-        { id: 3, userId: 3, name: 'test3', description: 'test description' },
-        { id: 4, userId: 4, name: 'test1', description: 'test description' },
-        { id: 5, userId: 5, name: 'test2', description: 'test description' },
-        { id: 6, userId: 6, name: 'test3', description: 'test description' },
-        { id: 7, userId: 7, name: 'test1', description: 'test description' },
-        { id: 8, userId: 8, name: 'test2', description: 'test description' },
-        { id: 9, userId: 9, name: 'test3', description: 'test description' },
-        { id: 10, userId: 10, name: 'test1', description: 'test description' },
-        { id: 11, userId: 11, name: 'test2', description: 'test description' },
-        { id: 12, userId: 12, name: 'test3', description: 'test description' },
-        { id: 13, userId: 13, name: 'test1', description: 'test description' },
-        { id: 14, userId: 14, name: 'test2', description: 'test description' },
-        { id: 15, userId: 15, name: 'test3', description: 'test description' },
-        { id: 16, userId: 16, name: 'test1', description: 'test description' },
-        { id: 17, userId: 17, name: 'test2', description: 'test description' },
-        { id: 18, userId: 18, name: 'test3', description: 'test description' },
-        { id: 19, userId: 19, name: 'test1', description: 'test description' },
-        { id: 20, userId: 20, name: 'test2', description: 'test description' },
-        { id: 21, userId: 21, name: 'test3', description: 'test description' },
-        { id: 22, userId: 22, name: 'test1', description: 'test description' },
-        { id: 23, userId: 23, name: 'test2', description: 'test description' },
-        { id: 24, userId: 24, name: 'test3', description: 'test description' },
-        { id: 25, userId: 25, name: 'test1', description: 'test description' },
-        { id: 26, userId: 26, name: 'test2', description: 'test description' },
-        { id: 27, userId: 27, name: 'test3', description: 'test description' },
-        { id: 28, userId: 28, name: 'test1', description: 'test description' },
-        { id: 29, userId: 29, name: 'test2', description: 'test description' },
-        { id: 30, userId: 30, name: 'test3', description: 'test description' }
+        { id: 1, userId: 1, img: 1, name: 'Tina', description: 'Wants to learn everything' },
+        { id: 2, userId: 2, img: 2, name: 'Gina', description: 'Here for free pie' },
+        { id: 3, userId: 3, img: 3, name: 'Lina', description: 'I learn the good English you teach good' },
+        { id: 4, userId: 4, img: 4, name: 'Zena', description: 'I do not know what to say. I am too shy' },
+        { id: 5, userId: 5, img: 5, name: 'Nina', description: 'Is this a good way to learn?' },
+        { id: 6, userId: 6, img: 6, name: 'Terry', description: 'I know a lot and can teach a lot for those that need help' },
+        { id: 7, userId: 7, img: 7, name: 'Gerry', description: 'Do I have to delete "test description" in my bio? How yous all doing!' },img: 8, 
+        { id: 8, userId: 8, img: 8, name: 'Merry', description: 'I was a teacher and love language exchanges' },
+        { id: 9, userId: 9, img: 9, name: 'Lerry', description: 'I cannt spel so this is had' },
+        { id: 10, userId: 10, img: 10, name: 'Rolly', description: 'I cannt spel so this is had' },
+        { id: 11, userId: 11, img: 11, name: 'Trolly', description: 'I am just a typical online troll that needs to learn new words. Please help this horrib... disgus.... abhorr.... yucky troll' }
       ])
     })
 }
