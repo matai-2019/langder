335a05ec153101eafc93fd4e472c81fc5eb3c277
noel <noelsoong@live.com>
2019-08-02 16:07:50 +1200

    Obtain Redux state using useSelector Hook

--- client/components/PotMatches.jsx
+++ client/components/PotMatches.jsx
@@ -1,56 +1,80 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Button, Card } from 'semantic-ui-react'
 
 import Profile from './Profile'
 import LikeControls from './LikeControls'
 
-import { likePotMatch, nextPotMatch, fetchPotMatches } from '../actions/potMatches'
+import { likePotMatch, fetchPotMatches, nextPotMatch } from '../actions/potMatches'
 
 import '../styles/pot.css'
-class PotMatches extends React.Component {
-  componentDidMount () {
-    const userId = this.props.user.id
-    this.props.dispatch(fetchPotMatches(userId))
+
+const PotMatches = (props) => {
+  // const { user } = props
+  const dispatch = useDispatch()
+
+  const user = useSelector(state => state.user)
+  const pots = useSelector(state => state.potMatches)
+  // const [pots, setPots] = useState(potMatches)
+  const [activePot, setActivePot] = useState(null)
+  const [nextPot, setNextPot] = useState(null)
+  const [interacted, setInteracted] = useState([])
+
+  useEffect(() => {
+    if (pots.length <= 0) {
+      dispatch(fetchPotMatches(user.id))
+    }
+    if (!pots.pending) {
+      setActivePot(pots[0])
+      setNextPot(pots[1])
+    }
+  })
+
+  const cycleCards = (id) => (e) => {
+    console.log(e.target.name)
+    switch (e.target.name) {
+      case 'dislike-button':
+        dispatch(nextPotMatch())
+        break
+      case 'like-button':
+        dispatch(likePotMatch(id, activePot.userId))
+        break
+      default:
+        break
+    }
+
+    setInteracted([...interacted, activePot])
+    // setPots(pots.slice(0, 2))
+    setActivePot(pots[0])
+    setNextPot(pots[1])
   }
 
-  render () {
-    const { activePot, nextPot, dispatch, user: { id } } = this.props
-    return (
+  return (
     <>
       <div className="pot">
-        {activePot && <Profile user={activePot} className='active-card'>
-          <LikeControls>
-            <Button icon='close' size="massive" circular
-              color="red"
-              className="dislike-button"
-              onClick={() => dispatch(nextPotMatch())} />
-            <Button icon='like' size="massive" circular
-              color="pink"
-              className="like-button"
-              onClick={() => dispatch(likePotMatch(id, activePot.userId))} />
-          </LikeControls>
-        </Profile>}
-        {nextPot && <Profile user={nextPot} className=' next-card' />}
-
-        {this.props.potMatches.length > 0 &&
-       <Card className="base-card" onClick={() => dispatch(fetchPotMatches())}>
-         <Card.Header content="Sorry About that you can swipe another time!" />
-       </Card>
+        {activePot &&
+          <Profile user={activePot} className='active-card'>
+            <LikeControls>
+              <Button icon='close' size="massive" circular
+                color="red"
+                name="dislike-button"
+                className="dislike-button"
+                onClick={cycleCards()} />
+              <Button icon='like' size="massive" circular
+                color="pink"
+                name="like-button"
+                className="like-button"
+                onClick={cycleCards(user.id)} />
+            </LikeControls>
+          </Profile>}
+        {nextPot ? <Profile user={nextPot} className=' next-card' />
+          : (<Card className="base-card" onClick={() => dispatch(fetchPotMatches(user.id))}>
+            <Card.Header content="Sorry About that you can swipe another time!" />
+          </Card>)
         }
       </div>
     </>
-    )
-  }
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.user,
-    potMatches: state.potMatches,
-    activePot: state.potMatches[0],
-    nextPot: state.potMatches[1]
-  }
-}
-
-export default connect(mapStateToProps)(PotMatches)
+export default PotMatches
