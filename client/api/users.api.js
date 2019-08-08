6773f91d5950ef71aa08c38324f3a83578600c64
noel <noelsoong@live.com>
2019-08-08 19:46:41 +1200

    Add Update languages super agent call

--- client/api/users.api.js
+++ client/api/users.api.js
@@ -1,6 +1,7 @@
 import request from 'superagent'
 
 export function getPotMatches (userId) {
+  console.log('get potmatches')
   return request.get(`/api/v1/users/${userId}/pot`)
 }
 
@@ -13,6 +14,12 @@ export function getLanguages (id) {
   return request.get(`/api/v1/users/${id}/languages`)
 }
 
+export function updateLanguages ({ userId, languages }) {
+  return request.put(`/api/v1/users/${userId}/languages`)
+    .send(languages)
+}
+
 export function getMatches (id) {
+  console.log('get matches')
   return request.get(`/api/v1/users/${id}/matches`)
 }
