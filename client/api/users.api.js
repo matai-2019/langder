fe95d712ee86e5ddca8523d6571854acde8bf14f
noel <noelsoong@live.com>
2019-08-08 15:15:53 +1200

    Add new usersAPI superagent

---
+++ client/api/users.api.js
@@ -0,0 +1,11 @@
+import request from 'superagent'
+
+export function getPotMatches (userId) {
+  return request.get(`/api/v1/users/${userId}/pot`)
+}
+
+export function like (userId, likedId) {
+  request
+    .post(`/api/v1/users/${userId}/likes`)
+    .send({ likedId })
+}
