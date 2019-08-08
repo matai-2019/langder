3c67bf99a71b1491e64f1d04cbe06127b5c0b3fb
noel <noelsoong@live.com>
2019-08-08 19:48:03 +1200

    Add update profile super agent call

--- client/api/profiles.api.js
+++ client/api/profiles.api.js
@@ -1,5 +1,12 @@
 import request from 'superagent'
 
+const url = '/api/v1/profiles/'
+
 export function getProfile (id) {
-  return request.get('/api/v1/profiles/' + id)
+  return request.get(url + id)
+}
+
+export function updateProfile ({ userId, name, description }) {
+  return request.put(url + userId)
+    .send({ name, description })
 }
