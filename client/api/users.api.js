4671ed22730e05897580d0390609f40f1cb59324
noel <noelsoong@live.com>
2019-08-08 20:17:48 +1200

    Refactor: add uri constant

--- client/api/users.api.js
+++ client/api/users.api.js
@@ -1,30 +1,32 @@
 import request from 'superagent'
 
+const uri = '/api/v1/users'
+
 export function getPotMatches (userId) {
   console.log('get potmatches')
-  return request.get(`/api/v1/users/${userId}/pot`)
+  return request.get(`${uri}/${userId}/pot`)
 }
 
 export function addUser (user) {
-  return request.post('/api/v1/users')
+  return request.post(uri)
     .send(user)
 }
 
 export function like (userId, likedId) {
-  request.post(`/api/v1/users/${userId}/likes`)
+  request.post(`${uri}/${userId}/likes`)
     .send({ likedId })
 }
 
 export function getLanguages (id) {
-  return request.get(`/api/v1/users/${id}/languages`)
+  return request.get(`${uri}/${id}/languages`)
 }
 
 export function updateLanguages ({ userId, languages }) {
-  return request.put(`/api/v1/users/${userId}/languages`)
+  return request.put(`${uri}/${userId}/languages`)
     .send(languages)
 }
 
 export function getMatches (id) {
   console.log('get matches')
-  return request.get(`/api/v1/users/${id}/matches`)
+  return request.get(`${uri}/${id}/matches`)
 }
